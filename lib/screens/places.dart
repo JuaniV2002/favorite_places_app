92ee2685b718d0eca9a233b652b4867b4fce390f
JuaniV2002 <juani2002villa@icloud.com>
2024-06-09 20:09:00 -0300

    first add place screen implementation

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places_app/widgets/places_list.dart';
+import 'package:favorite_places_app/screens/add_place.dart';
 
 class PlacesScreen extends StatelessWidget {
   const PlacesScreen({super.key});
@@ -13,7 +14,13 @@ class PlacesScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
           ),
         ],
       ),
