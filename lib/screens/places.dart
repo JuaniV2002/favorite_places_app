1523e54de2bc705ff1333e97d1e7adf5b6a91dc4
JuaniV2002 <juani2002villa@icloud.com>
2024-06-09 19:58:43 -0300

    places screen first layout

---
+++ lib/screens/places.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places_app/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: PlacesList(places: []),
+    );
+  }
+}
