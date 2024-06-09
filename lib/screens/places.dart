a07da90a09ef6479097e9f11075e53723d988c87
JuaniV2002 <juani2002villa@icloud.com>
2024-06-09 20:26:58 -0300

    adding places logic done

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,13 +1,17 @@
+import 'package:favorite_places_app/providers/user_places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favorite_places_app/widgets/places_list.dart';
 import 'package:favorite_places_app/screens/add_place.dart';
 
-class PlacesScreen extends StatelessWidget {
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces =  ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your places'),
@@ -24,7 +28,9 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: PlacesList(places: []),
+      body: PlacesList(
+        places: userPlaces,
+      ),
     );
   }
 }
