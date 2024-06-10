a221206d441768e597d1020d49fe0a41f12ffae6
JuaniV2002 <juani2002villa@icloud.com>
2024-06-10 11:38:24 -0300

    added map screen to manually select a location

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -7,6 +7,12 @@ class PlacesDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final lng = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyBhanenYpqY1_KqhQU3Ua-jf9s-FGlsJFc';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +31,40 @@ class PlacesDetailScreen extends StatelessWidget {
             width: double.infinity,
             height: double.infinity,
             fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.black54,
+                        Colors.transparent,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    place.location.address,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
